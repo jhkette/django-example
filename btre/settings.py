41e26251f18543fecbe34e77c6bb901b578b1d1a
khenryjoe <joseph.ketterer@gmail.com>
2020-05-15 11:11:04 +0100

    pagination

--- btre/settings.py
+++ btre/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -141,5 +142,5 @@ STATICFILES_DIRS = [
 ]
 
 # MEDIA FOLDER SETTINGS
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media');
-MEDIA_URL = './media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+# we need to import these modules for pagination
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
-
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    # orderby list data - adding a minus so earliest is first
+    listings = Listing.objects.all().order_by('-list_date')
+    # For pagingation please see django docs
+    # https://docs.djangoproject.com/en/3.0/topics/pagination/
+    paginator = Paginator(listings, 3)
+
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings': paged_listings
+    }
+    
+    return render(request, 'listings/listings.html', context)
 
 
-
-def listing(request):
+#  we are adding the listing_id, this is in the urls as a parameter 
+# so we need to pass it in here
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
